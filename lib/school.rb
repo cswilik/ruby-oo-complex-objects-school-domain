4811202ad233b854ce93c5b1df7587f2a620d2b3
CSWILIK <cswilik@gmail.com>
2020-10-29 12:46:17 -0400

    commit tree 0ef167e6c0d2761231efb69c0a98a9a5ce0d7069

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,32 @@
 # code here!
+require 'pry'
+class School 
+    attr_reader :name, :roster
+
+    def initialize(name)
+        @name = name
+        @roster = {}
+    end 
+
+    def add_student(student, grade)
+        if roster.has_key?(grade)
+            @roster[grade] << student
+        else
+            @roster[grade] = []
+            @roster[grade] << student 
+        end 
+    end 
+
+    def grade(grade)
+        self.roster[grade]
+    end 
+
+    def sort
+        self.roster.select { |grade, students| students.sort! }
+    end
+
+
+end 
+
+
+
